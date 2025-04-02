c24af18120efec4cb7c4c27e7d7c6bfbdc1a0876
sunsally <sunsally@localhost>
2025-04-02 15:17:36 -0500

    :)

--- src/studio8/Quiz.java
+++ src/studio8/Quiz.java
@@ -35,20 +35,13 @@ public class Quiz {
 	
 	public static void main(String[] args) {
 		Question q1 = new Question("What is 2+2?", "4", 1);
-		String[] options = {"Paris", "London", "Berlin", "Madrid"};
-		MultipleChoiceQuestion q2 = new MultipleChoiceQuestion(
-			"What is the capital of France?", 
-			"1", 
-			2, 
-			options);
-		SelectAllQuestion q3 = new SelectAllQuestion(
-			"Select all European capitals:", 
-			"13", 
-			options);
-		
-		Question[] questions = {q1, q2, q3};
+		String[] options = { "Paris", "London", "Berlin", "Madrid" };
+		MultipleChoiceQuestion q2 = new MultipleChoiceQuestion("What is the capital of France?", "1", 2, options);
+		SelectAllQuestion q3 = new SelectAllQuestion("Select all European capitals:", "13", options);
+
+		Question[] questions = { q1, q2, q3 };
 		Quiz quiz = new Quiz(questions);
-		
+
 		Scanner scanner = new Scanner(System.in);
 		quiz.takeQuiz(scanner);
 		scanner.close();
